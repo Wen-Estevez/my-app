c39e3ea2eba9c1c9851f4a0c383205745f179412
Wen <westevez04@gmail.com>
2022-12-20 23:32:20 -0500

    estilos busqueda

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,18 @@
 import React, { useContext, useEffect, useState } from "react";
+import { Redirect } from "react-router-dom";
 import Styled from "styled-components";
 import { AuthContext } from "../App";
 import axios from "axios";
-import { useHistory } from "react-router-dom";
 
 export default function Home() {
   const { state, dispatch } = useContext(AuthContext);
-  const history = useHistory();
-  if (!state.isLoggedIn) {  history.push("/login") }
 
   const { avatar_url, public_repos, followers, following, repos_url, login } = state.user
+
   const [repos, setRepos] = useState([]);
   const [word, setWord] = useState([]);
 
-  const fetchData = async () => {
+  const fetchData = async () =>{
     const fetchRepos = await axios.get(repos_url);
     setRepos(fetchRepos.data)
   }
@@ -24,21 +23,22 @@ export default function Home() {
   }
 
   const handleFilter = () => {
-    const search = repos.filter((repo) => repo.name.toLowerCase().includes(word.toLowerCase()) );
+    const search = repos.filter((repo)=>repo.name===word);
     setRepos(search)
   }
 
-  const handleInput = (e) => { setWord(e.target.value) }
+  const handleInput =(e)=>{setWord(e.target.value)}
 
   useEffect(() => {
     fetchData()
   }, [])
-
+  
+  if (!state.isLoggedIn) {return <Redirect to="/login" />}
   return (
     <Wrapper>
       <div className="container">
         <button onClick={handleLogout}>Cerrar sesion</button>
-        <input onChange={handleInput} />
+        <input onChange={handleInput} placeholder="Buscar repositorio..." />
         <button onClick={handleFilter}>Buscar</button>
         <button onClick={fetchData}>Borrar</button>
         <div>
@@ -66,6 +66,15 @@ const Wrapper = Styled.section`
   flex-direction: column;
   height: 100vh;
   font-family: Arial;  
+  input{
+    width: 50%;
+    padding: 8px;
+    font-size: 15px;
+    margin: auto;
+    border-radius: 15px;
+    border: 1px solid #ccc;
+    box-shadow: 3px 2px 5px 0px rgba(179,179,179,1);
+  }
   button{
     all: unset;
     width: 100px;
@@ -83,6 +92,7 @@ const Wrapper = Styled.section`
       -webkit-background-clip: text;
       -webkit-text-fill-color: transparent;
       border: 1px solid #013E5C;
+      border-radius: 5px;
     }
   }
   >div{
